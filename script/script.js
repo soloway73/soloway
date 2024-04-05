b1078dd894e0c0f3d984b74ea0e87ffbdb8a0a85
Ilyas Khisyametdinov <hopi@mail.ru>
2024-04-05 19:13:37 +0400

    LAND-11 burger added

---
+++ script/script.js
@@ -0,0 +1,20 @@
+// Меню бургер
+const iconMenu = document.querySelector(".menu-icon");
+const menuBody = document.querySelector(".links");
+
+if (iconMenu) {
+  iconMenu.addEventListener("click", function (e) {
+    document.body.classList.toggle("_lock");
+    menuBody.classList.toggle("_active");
+    iconMenu.classList.toggle("_active");
+  });
+}
+
+let links = menuBody.querySelectorAll("a");
+
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function () {
+    menuBody.classList.remove("_active");
+    iconMenu.classList.remove("_active");
+  });
+}
